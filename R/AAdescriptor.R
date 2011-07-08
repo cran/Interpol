0d76c413c71560539235ba22e2aed4fd236b7e63
Dominik Heider <dominik.heider@uni-due.de>
2011-07-08 00:00:00 +0000

    version 1.3.1

--- R/AAdescriptor.R
+++ R/AAdescriptor.R
@@ -1,5 +1,5 @@
 AAdescriptor <-
-function(data,descriptor=151,normalize=0){
+function(data,descriptor=151,normalize=0) {
 
 	#################################################catch missing or bad input
 	if(missing(data))
@@ -7,10 +7,10 @@ function(data,descriptor=151,normalize=0){
         else
           if(!(is.character(data)))
             stop("invalid argument: data argument is required to be a vector of amino acids")
-	if(!is.numeric(descriptor) | descriptor > 532 | descriptor < 1)
-	  stop("invalid argument: descriptor is required to be 1 <= n <= 532")
+	if(!is.numeric(descriptor) | descriptor > 533 | descriptor < 1)
+	  stop("invalid argument: descriptor is required to be 1 <= n <= 533")
 	if(!is.numeric(normalize) | normalize > 2 | normalize < 0)
-	  stop("invalid argument: descriptor is required to be 1 <= n <= 532")
+	  stop("invalid argument: normalization argument is required to be {1,2,3}")
 	if(missing(descriptor))
 		descriptor = 151
 	if(missing(normalize))
@@ -19,42 +19,30 @@ function(data,descriptor=151,normalize=0){
 	################################################Encoding
 	
 	#load("descriptors.RData")
-	data(list)
-	indices <- list[,1]
-	list = list[,-1]
+	data(AAindex)
+	indices <- AAindex[,1]
+	AAindex = AAindex[,-1]
 
 	descriptor = round(descriptor)
 	normalize = round(normalize) 
+	max_value = max(AAindex[descriptor,])
 
-	max_value = max(list[descriptor,])
+	################################################Determine normalization function to use
+	if(normalize==0) { norm <- function(data,max_value) { return(data) } }
+	if(normalize==1) { norm <- function(data,max_value) { return(data/max_value) } }
+	if(normalize==2) { norm <- function(data,max_value) { return((data+max_value)/(max_value*2)) } }
 
-	data = unlist(strsplit(data,""))
-	data.encoded = c()
+	#data.encoded = list()
+	data.encoded <- vector("list", length(data))
+	for(n in 1:length(data)) {
+		data.encoded.tmp = c()
+		data.tmp = unlist(strsplit(data[n],""))
+		for(m in 1:length(data.tmp)){ data.encoded.tmp = c(data.encoded.tmp, AAindex[descriptor, data.tmp[m]]) }
+		data.encoded[[n]] <- norm(data.encoded.tmp, max_value)
+	}
+	names(data.encoded) <- names(data)
 
-	for(n in 1:length(data)){
-		data.encoded = c(data.encoded, list[descriptor, data[n]])		
-	}
-	################################################Normalization
-
-	if(normalize==0){
-		print(paste("data encoded with ", indices[descriptor], sep=""))
-		return(data.encoded)	
-	}
-	if(normalize==1){
-		tmp = c()
-		for(i in 1:length(data.encoded)){
-			tmp = c(tmp, (data.encoded[i]/max_value))
-		}
-		print(paste("data encoded with ", indices[descriptor], sep=""))
-		return(tmp)
-	}
-	if(normalize==2){
-		tmp = c()
-		for(i in 1:length(data.encoded)){
-			tmp = c(tmp, ((data.encoded[i]+max_value)/(2*max_value)))
-		}
-		print(paste("data encoded with ", indices[descriptor], sep=""))
-		return(tmp)
-	}
+	return(data.encoded)
+	
 }
 
--- R/Interpol.R
+++ R/Interpol.R
@@ -1,98 +1,117 @@
 Interpol <-
-function(data,dims,method="linear"){
+function(data,dims,method="linear") {
 
 	## catch missing or bad input
 	if(missing(data))
           stop("data argument missing")
         else
-          if(!is.vector(data))
-            stop("invalid argument: data argument is required to be a vector of D features")
+          #if(!is.vector(data))
+          #  stop("invalid argument: data argument is required to be a vector of D features")
         if(!is.numeric(dims) | (dims < 2))
           stop("invalid argument: target dimension is required to be a positive integer value > 1")
-	if(!is.character(method) | !(method=="linear" | method=="spline" | method=="natural" | method =="fmm" | method =="periodic"))
+	if(!is.character(method) | !(method=="linear" | method=="spline" | method=="natural" | method =="fmm" | method =="periodic" | method =="average"))
 	  stop("invalid argument: method is required to be linear, spline, natural, fmm or periodic")
 	if(missing(method))
 		method = "linear"
+
+	max.length = 0
+	for(m in 1:length(data)){
+		y = unlist(data[[m]])
+		if(max.length < length(y)){
+			max.length = length(y)	
+		}
+	}
+	################################averaging
+	#if(method == "average" | dims < (max.length / 2)) {
+	if(method == "average") {
+		output <- c()
+		length = dims
+		
+		for(n in 1:length(data)) {
+			y = unlist(data[[n]])
+			x = seq(1:length(y))
+			factor = (length(y))/(length)
+			steps = (1:length)*factor
+			#print(steps)
+			values = mean(y[1:floor(steps[1])])
+			for(i in 1:(length(steps)-1)){
+				tmp = mean(y[floor(steps[i]):floor(steps[i+1])])
+				#print(paste("unten: ", floor(steps[i])))
+				#print(paste("oben: ", floor(steps[i+1])))
+				#print(paste("werte:", y[floor(steps[i]):floor(steps[i+1])]))
+				#print(tmp)
+				#print("-------------------------------")
+				values = c(values, tmp)
+			}			
+			output <- rbind(output,values)
+		}
+		colnames(output) <- 1:length(output[1,])
+		for(i in 1:length(output[1,])){
+			colnames(output)[i] <- paste("V", i, sep="")
+		}
+		rownames(output) <- 1:length(output[,1])
+
+		NAS = which(is.na(output))
+		output[NAS] = output[NAS-1]
+		return(output)
+	}
 	
 	################################linear interpolation
-	if(method == "linear"){
-		list = data
+	if(method == "linear") {
 
+		output <- c()
 		length = dims
- 		if(length == 2){return(c(list[1], list[length(list)]))}
-		factor = (length(list) - 1) / (length - 1)
 
-		output = list[1]
+ 		if(length == 2) {
+			for(n in 1:length(data)) { 
+				y = unlist(data[[n]])
+				cut1 = 1
+				cut2 = floor(length(y) / 2)
+				cut3 = length(y)
+				value1 = mean(y[cut1:cut2])
+				value2 = mean(y[cut2:cut3])
+				output <- rbind(output, c(value1, value2)) 
+			}
+		}
+		else {
+			output <- c()
+			for(n in 1:length(data)) {
+				list <- unlist(data[[n]])
+				if(!(length(list)==length)){
 
-		for(i in 1:(length-2)){
-			normIndex = factor * i
-			floorIndex = floor(normIndex)+1
-			tmp = list[floorIndex]*(floorIndex+1-normIndex) + list[floorIndex+1] * (normIndex-floorIndex)
-			output = c(output, tmp)			
+					factor = (length(list) - 1) / (length - 1)
+					output.tmp = list[1]
+					for(i in 1:(length-2)){
+						normIndex = factor * i
+						floorIndex = floor(normIndex)+1
+						tmp = list[floorIndex]*(floorIndex+1-normIndex) + list[floorIndex+1] * (normIndex-floorIndex)
+						output.tmp = c(output.tmp, tmp)			
+					}
+					output.tmp = c(output.tmp, list[length(list)])
+				}else{output.tmp = list}
+				output <- rbind(output, output.tmp)
+			}
 		}
-		output = c(output, list[length(list)])	
-		print(paste("data is ", method, " interpolated to length ", dims, sep=""))
+
+		rownames(output) <- names(data)
 		return(output)
+
 	}
-	################################spline interpolation
-	if(method == "spline"){
-		y = data	
-		x = seq(1:length(y))
-		f_fmm = splinefun(x,y, method="monoH.FC")
-		factor = length(y)/dims
-		tmp = c()	
+	################################various spline interpolations
+	if(method == "spline") { method = "monoH.FC" } #replaces spline with monoH.FC, other methods are used as is.
+
+	output <- c()
+	length = dims
 		
-		for(n in 1:dims){
-			tmp = c(tmp, n*factor)
-		}
-		output = f_fmm(tmp)
-		print(paste("data is ", method, " interpolated to length ", dims, sep=""))
-		return(output)
-	}
-	################################fmm interpolation
-	if(method == "fmm"){
-		y = data	
+	for(n in 1:length(data)) {
+		y = unlist(data[[n]])
 		x = seq(1:length(y))
-		f_fmm = splinefun(x,y, method="fmm")
-		factor = length(y)/dims
-		tmp = c()	
-		
-		for(n in 1:dims){
-			tmp = c(tmp, n*factor)
-		}
-		output = f_fmm(tmp)
-		print(paste("data is ", method, " interpolated to length ", dims, sep=""))
-		return(output)
+		f_fmm = splinefun(x,y, method=method)
+		factor = length(y)/length
+		output <- rbind(output,f_fmm((1:length)*factor))
 	}
-	################################natural interpolation
-	if(method == "natural"){
-		y = data	
-		x = seq(1:length(y))
-		f_fmm = splinefun(x,y, method="natural")
-		factor = length(y)/dims
-		tmp = c()	
-		
-		for(n in 1:dims){
-			tmp = c(tmp, n*factor)
-		}
-		output = f_fmm(tmp)
-		print(paste("data is ", method, " interpolated to length ", dims, sep=""))
-		return(output)
-	}
-	################################periodic interpolation
-	if(method == "periodic"){
-		y = data	
-		x = seq(1:length(y))
-		f_fmm = splinefun(x,y, method="periodic")
-		factor = length(y)/dims
-		tmp = c()	
-		
-		for(n in 1:dims){
-			tmp = c(tmp, n*factor)
-		}
-		output = f_fmm(tmp)
-		print(paste("data is ", method, " interpolated to length ", dims, sep=""))
-		return(output)
-	}
+	names(output) <- names(data)
+	return(output)
+
 }
 
